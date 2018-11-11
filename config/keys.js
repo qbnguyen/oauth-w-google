22c427892f32ccde3ccf4c6a5cef1c05d4260aa3
Quy Nguyen <nguyenqb24@gmail.com>
2018-11-11 16:46:32 -0500

    adding

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: '219205541710-i69iselndtusj0hh4ns8cc15duohihlb.apps.googleusercontent.com',
+        clientSecret: 'jzJiR_leFtwC6cEJc7R3kqEw'
+    }
+};
